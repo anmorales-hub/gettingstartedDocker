83ae1d6696dd76d6f3b5d1e0195a3601ce0f0f97
anmorales-hub <moralesand2018@gmail.com>
2020-02-25 15:15:36 -0500

    Task: Add RNG

---
+++ Statistics/randNums.py
@@ -0,0 +1,39 @@
+import random
+
+class RandGen:
+
+    def randNum(self, lower, upper):
+        return random.randint(lower, upper)
+
+    def randNum_seed(self, seed, lower, upper):
+        random.seed(seed)
+        return self.randNum(lower, upper)
+
+    def randList_seed(self, length, seed, lower, upper):
+        list =[]
+        while len(list) != length:
+            list.append(self.randNum_seed(seed, lower, upper))
+        return list
+
+    def fromList(self, list):
+        return random.choice(list)
+
+    def fromList_seed(self, seed, list):
+        random.seed(seed)
+        return self.fromList(list)
+
+    def fromList2(self, n, list):
+        temp = []
+        while len(temp) < n:
+            temp.append(self.fromList(list))
+        return temp
+
+    def fromList2_seed(self, seed, n, list):
+        random.seed(seed)
+        self.fromList2(n,list)
+
+print(RandGen.randNum(0, 20))
+
+
+
+
