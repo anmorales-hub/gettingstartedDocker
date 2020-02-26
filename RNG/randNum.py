b0938203cfce158df6355fac16670b0c433cedc5
anmorales-hub <moralesand2018@gmail.com>
2020-02-25 19:22:27 -0500

    Task: Add randNumSeed function

--- RNG/randNum.py
+++ RNG/randNum.py
@@ -5,3 +5,9 @@ class RandNum:
     @staticmethod
     def randNum(lower, upper):
         return random.randint(lower, upper)
+
+    @staticmethod
+    def randNumSeed(seed,lower,upper):
+        random.seed(seed)
+        return RandNum.randNum(lower,upper)
+
--- Tests/test_Random.py
+++ Tests/test_Random.py
@@ -7,6 +7,12 @@ class MyTestCase(unittest.TestCase):
         result = RandNum.randNum(0, 10)
         self.assertEqual(int, type(result))
 
+    def test_randNumSeed(self):
+        result1 = RandNum.randNumSeed(4, 0, 10)
+        result2 = RandNum.randNumSeed(4, 0, 10)
+        compare = result1 == result2
+        self.assertEqual(True, compare)
+
 
 
 if __name__ == '__main__':
